f138428044f554de4def2d3c76f05df68919ff49
Ernald Nicolas <ernald.nicolas@gmail.com>
2021-05-28 17:20:01 -0400

    Let's try this

--- app.js
+++ app.js
@@ -112,9 +112,8 @@ app.get(/^\/([0-9a-z]{1,5})$/, routes.invite);
 app.get('/.well-known/acme-challenge/:content', function(req, res) {
     res.send('oq-OsulF5JlEYofdMC9WFMH3j8PQDUwJThwOdHHab54.n-PCJWon7UBcvnOb3hiQInfn15uFqQDNBA0UWFBO35w')
 });
-app.enable('trust proxy');
-app.use((req, res, next) => {
-    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+app.get('*', function(req, res) {
+    res.redirect('https://' + req.headers.host + req.url);
 });
 
 // Listen on the port.
